99afd6233b171a10e3ab711854399f6329ad6757
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-03-22 12:48:47 +0200

    Add Apollo

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,6 +4,7 @@ import GlobalStyles from '../Styles/GlobalStyles'
 import Theme from '../Styles/Theme';
 import Router from './Router';
 
+
 export default () => (
   <ThemeProvider theme={Theme}>
   <>
