3bc723426de606947b45757d239d58c984956c52
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-03-22 13:11:26 +0200

    Auth UI. Vol 1

--- src/Styles/Theme.js
+++ src/Styles/Theme.js
@@ -11,8 +11,8 @@ export default {
   darkBlueColor: "#003569",
   boxBorder: BOX_BORDER,
   borderRadius: BORDER_RADIUS,
-  whiteBox: `${BOX_BORDER};
-             ${BORDER_RADIUS};
+  whiteBox: `border: ${BOX_BORDER};
+             border-radius: ${BORDER_RADIUS};
              background-color: white;
             `
 }
