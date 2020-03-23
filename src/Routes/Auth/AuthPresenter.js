6499d53586ac356dedb09af877b36d1b7845af06
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-03-23 12:59:22 +0200

    Create account mutation. Part 1

--- src/Routes/Auth/AuthPresenter.js
+++ src/Routes/Auth/AuthPresenter.js
@@ -53,18 +53,18 @@ export default ({
   firstName,
   lastName,
   email,
-  onLogin
+  onSubmit
 }) => (
   <Wrapper>
     <Form>
       {action === "logIn" ? (
-        <form onSubmit={onLogin}>
+        <form onSubmit={onSubmit}>
           <Input placeholder={"Email"} {...email} type="email" />
           
           <Button text={"Log in"}/>
         </form>
       ) : (
-        <form onSubmit={onLogin}>
+        <form onSubmit={onSubmit}>
           <Input placeholder={"First name"} {...firstName} />
           <Input placeholder={"Last name"} {...lastName} />
           <Input placeholder={"Email"} {...email} type="email" />
